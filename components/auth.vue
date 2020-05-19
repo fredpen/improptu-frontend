a7dbd232e825ad0cf994fb047e21a9fce429644b
Oladipupo Fredrick <fredrickksola@gmail.com>
2020-05-19 21:37:05 +0100

    home page set up

---
+++ components/auth.vue
@@ -0,0 +1,114 @@
+<template>
+        <v-row fluid align="center" justify="center">
+            <v-col cols="11" class="mt-12">
+
+                <!-- login -->
+                <template  v-if="isLogin" >
+                    <v-card class="elevatsion-12">
+                        <v-toolbar color="primary" dark flat>
+                            <v-toolbar-title>Login</v-toolbar-title>
+                            <v-spacer></v-spacer>
+                        </v-toolbar>
+                        <v-card-text>
+                            <v-form>
+                                <v-text-field
+                                    label="Login"
+                                    name="login"
+                                    type="text"
+                                ></v-text-field>
+
+                                <v-text-field
+                                    id="password"
+                                    label="Password"
+                                    name="password"
+                                    type="password"
+                                ></v-text-field>
+                            </v-form>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="primary">Login</v-btn>
+                        </v-card-actions>
+
+                    
+                    </v-card>
+                    <p @click="isLogin = false">New user? Create an account here</p>
+                </template>
+
+                <!-- register -->
+                <v-card v-else class="elevatsion-12">
+                    <v-toolbar color="primary" dark flat>
+                        <v-toolbar-title>Register</v-toolbar-title>
+                        <v-spacer></v-spacer>
+                    </v-toolbar>
+                    <v-card-text>
+                        <v-form>
+                            <v-text-field
+                                label="name"
+                                name="name"
+                                type="text"
+                            ></v-text-field>
+                            <v-text-field
+                                label="email"
+                                name="email"
+                                type="email"
+                            ></v-text-field>
+                            <v-text-field
+                                label="phone number"
+                                name="phone number"
+                                type="phone"
+                            ></v-text-field>
+                            <v-text-field
+                                label="password"
+                                name="password"
+                                type="password"
+                            ></v-text-field>
+                            <v-select
+                                :items="items"
+                                attach
+                                chips
+                                label="Account Type"
+                            ></v-select>
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="primary">Register</v-btn>
+                    </v-card-actions>
+
+                    <p @click="isLogin = true">Already a User? Sign in here</p>
+                </v-card>
+            </v-col>
+        </v-row>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+
+export default {
+    name: "auth",
+
+    data() {
+        return {
+            isLogin : true, items : ['male', 'female'],
+        }
+    },
+
+    methods: {
+        // addTodo(e) {
+        //     this.$store.commit("bobo/add", e.target.value);
+        //     e.target.value = "";
+        // },
+        // ...mapMutations({
+        //     toggle: "todo/toggles" // todo is the modules/toggles is the methodname in store
+        // })
+    },
+
+    computed: {
+        // todos() { return this.$store.state.todo.list },
+        // todos() {
+        //     return this.$store.state.bobo.list;
+        // }
+    }
+};
+</script>
--- pages/fred.vue
+++ pages/fred.vue
@@ -2,10 +2,14 @@
     <v-layout column justify-center align-center>
         <v-flex xs12 sm8 md6>
             <v-card>
-                <v-card-title class="headline">
-                    Welcome to the Vuetify + Nuxt.js template
+                <v-card-title primary color="primary" class="headline">
+                    Welcome to the fred pageo
                 </v-card-title>
-                <v-card-text>
+
+                   <v-btn color="primary" nuxt to="/inspire">
+                        Continue
+                    </v-btn>
+                <!-- <v-card-text>
                     <p>
                         Vuetify is a progressive Material Design component
                         framework for Vue.js. It was designed to empower
@@ -13,7 +17,7 @@
                     </p>
                     <p>
                         For more information on Vuetify, check out the
-                        <a href="https://vuetifyjs.com" targ et="_blank">
+                        <a href="https://vuetifyjs.com" target="_blank">
                             documentation </a
                         >.
                     </p>
@@ -58,7 +62,7 @@
                     <v-btn color="primary" nuxt to="/inspire">
                         Continue
                     </v-btn>
-                </v-card-actions>
+                </v-card-actions> -->
             </v-card>
         </v-flex>
     </v-layout>
