7338fa76dbaadb66342926ddabba9a6478d564a0
Oladipupo Fredrick <fred@Oladipupos-MacBook-Pro.local>
2020-05-27 11:26:16 -0700

    working on the dashboard panel

---
+++ pages/dashboard.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-layout>
+    <v-row>
+      <v-col cols="2" class="d-none d-sm-flex">
+        <dashboard-panel></dashboard-panel>
+      </v-col>
+      <v-col>
+        <v-btn cols="10" block color="success">body</v-btn>
+      </v-col>
+    </v-row>
+    <br />
+    <!-- <v-btn block color="success">text</v-btn> -->
+  </v-layout>
+</template>
+
+
+<script>
+import dashboardPanel from "../components/dashboardPanel.vue";
+
+export default {
+  components: { dashboardPanel },
+  head() {
+    return {
+      title: "My Dashboard",
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: "description",
+          name: "description",
+          content: "Manage your dashboard"
+        }
+      ]
+    };
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,57 +2,106 @@
   <div>
     <v-layout column justify-center align-center>
       <v-flex>
-        <v-card>
-          <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
-          <v-card-text>
-            <p>
-              Vuetify is a progressive Material Design component
-              framework for Vue.js. It was designed to empower
-              developers to create amazing applications.
-            </p>
-            <p>
-              For more information on Vuetify, check out the
-              <a
-                href="https://vuetifyjs.com"
-                targ
-                et="_blank"
-              >documentation</a>.
-            </p>
-            <p>
-              If you have questions, please join the official
-              <a
-                href="https://chat.vuetifyjs.com/"
-                target="_blank"
-                title="chat"
-              >discord</a>.
-            </p>
-            <p>
-              Find a bug? Report it on the github
-              <a
-                href="https://github.com/vuetifyjs/vuetify/issues"
-                target="_blank"
-                title="contribute"
-              >issue board</a>.
-            </p>
-            <p>
-              Thank you for developing with Vuetify and I look forward
-              to bringing more exciting features in the future.
-            </p>
-            <div class="text-xs-right">
-              <em>
-                <small>&mdash; John Leider</small>
-              </em>
-            </div>
-            <hr class="my-3" />
-            <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-            <br />
-            <a href="https://github.com/nuxt/nuxt.js" target="_blank">Nuxt GitHub</a>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer />
-            <v-btn color="primary" nuxt to="/inspire">Continue</v-btn>
-          </v-card-actions>
-        </v-card>
+        <!-- how it works -->
+        <div>
+          <v-card-title class="text-center justify-center py-6">
+            <h1 class="display-3">How it works</h1>
+          </v-card-title>
+
+          <v-tabs v-model="tab" background-color="transparent" color="primary" centered>
+            <v-tab v-for="(item, index) in items" :key="index">{{ item.name }}</v-tab>
+          </v-tabs>
+
+          <v-tabs-items v-model="tab">
+            <v-tab-item v-for="(item, index) in items" :key="index">
+              <v-card color="basil" flat>
+                <v-card-text class="text-center">{{ item.text }}</v-card-text>
+              </v-card>
+            </v-tab-item>
+          </v-tabs-items>
+        </div>
+
+        <v-row style="justify-content:center" class="mt-4">
+          <v-btn style="display:blcock" large class="mx-2 myCenter" outlined center color="primary">
+            <NuxtLink to="/">Get Started</NuxtLink>
+          </v-btn>
+        </v-row>
+      </v-flex>
+
+      <!-- newest tasks -->
+      <v-flex style="margin-top:90px">
+        <!-- <h1>Recent Tasks</h1> -->
+        <v-sheet class="mx-auto" elevation="8" max-width="100%">
+          <v-slide-group
+            v-model="model"
+            class="pa-4"
+            :multiple="false"
+            :mandatory="false"
+            :show-arrows="true"
+            :center-active="false"
+          >
+            <v-slide-item v-for="n in 15" :key="n" v-slot:default="{ active, toggle }">
+              <v-card
+                :color="active ? 'transparent' : ''"
+                class="ma-4"
+                height="300"
+                width="300"
+                @click="toggle"
+              >
+                <v-img
+                  src="https://picsum.photos/350/165?random"
+                  height="80%"
+                  width="100%"
+                  class="grey darken-4"
+                ></v-img>
+                <v-card-title class="title">height</v-card-title>
+              </v-card>
+            </v-slide-item>
+          </v-slide-group>
+        </v-sheet>
+      </v-flex>
+
+      <!-- testemoinals -->
+      <v-flex>
+        <div style="margin-top:90px">
+          <h1 class="my-5">What people are saying about our service</h1>
+          <v-sheet light class="mx-auto" elevation="0" max-width="100%">
+            <v-card elevation="24" max-width="444" class="mx-auto">
+              <v-carousel
+                style="min-width:550px"
+                :show-arrows="true"
+                :continuous="true"
+                :cycle="false"
+                interval="4000"
+                :hide-delimiters="true"
+                delimiter-icon="mdi-minus"
+                height="300"
+              >
+                <v-carousel-item v-for="(testemoinal, i) in testemoinals" :key="i">
+                  <v-sheet light height="100%" tile>
+                    <v-row color="transparent" class="fill-height" align="center" justify="center">
+                      <div style="max-width:53%">
+                        <p>{{ testemoinal.text }}</p>
+                        <v-list two-line>
+                          <v-list-item>
+                            <v-list-item-avatar>
+                              <v-img :src="`/${testemoinal.image}`"></v-img>
+                            </v-list-item-avatar>
+
+                            <v-list-item-content class="textPrimary">
+                              <v-list-item-title class="text-capitalize">{{ testemoinal.name }}</v-list-item-title>
+                              <v-list-item-subtitle class="text-capitalize">{{ testemoinal.job }}</v-list-item-subtitle>
+                            </v-list-item-content>
+                          </v-list-item>
+                        </v-list>
+                      </div>
+                    </v-row>
+                  </v-sheet>
+                </v-carousel-item>
+              </v-carousel>
+            </v-card>
+          </v-sheet>
+        </div>
       </v-flex>
     </v-layout>
   </div>
@@ -61,7 +110,58 @@
 <script>
 export default {
   layout: "home",
-  components: {}
+  components: {},
+  data() {
+    return {
+      // products
+      model: null,
+
+      // testemoinals
+      testemoinals: [
+        {
+          text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+          name: "fredo",
+          job: "Software Engineer",
+          image: "test1.jpeg"
+        },
+        {
+          text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+          name: "Oladipupo Fredrick",
+          job: "Manager Engineer",
+          image: "test2.jpeg"
+        },
+        {
+          text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
+          name: "Caleb the boss",
+          job: "Manager Engineer",
+          image: "test3.jpeg"
+        }
+      ],
+
+      //   tab props
+      tab: null,
+      items: [
+        {
+          name: "Post task",
+          text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        },
+        {
+          name: "Task Assignment",
+          text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        },
+        {
+          name: "Review and Payment",
+          text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        }
+      ]
+    };
+  }
 };
 </script>
 
